574f4b17a2b6ca2f4b69f27ef46e5f5fb69e884e
Vince <vhorvath2010@gmail.com>
2022-06-24 22:44:36 -0400

    began messing with line plots

--- simple_plots.py
+++ simple_plots.py
@@ -5,17 +5,21 @@ ds = yt.load("data/GasSloshing/sloshing_nomag2_hdf5_plt_cnt_0150")
 
 print("list of gas fields: ", dir(ds.fields.gas))
 
-for normal in "xyz":
+#for normal in "xyz":
     # Create gas density slices in all three axes.
-    yt.SlicePlot(ds, normal, ("gas", "density"), width=(800.0, "kpc")).save()
+    #yt.SlicePlot(ds, normal, ("gas", "density"), width=(800.0, "kpc")).save()
 
     # Display non-weighted line integral of gas density
-    yt.ProjectionPlot(ds, normal, ("gas", "density")).save()
+    #yt.ProjectionPlot(ds, normal, ("gas", "density")).save()
 
     # Weighted line integral of gas density
-    yt.ProjectionPlot(ds, normal, ("gas", "density"), weight_field=("gas", "density")).save()
+    #yt.ProjectionPlot(ds, normal, ("gas", "density"), weight_field=("gas", "density")).save()
 
 # Create a phase plot of the temperature/density pairs by cell mass
-phase_plot = yt.PhasePlot(ds, ("gas", "density"), ("gas", "temperature"), ("gas", "cell_mass"), weight_field=None)
-phase_plot.set_unit(("gas", "cell_mass"), "Msun")
-phase_plot.save()
+#phase_plot = yt.PhasePlot(ds, ("gas", "density"), ("gas", "temperature"), ("gas", "cell_mass"), weight_field=None)
+#phase_plot.set_unit(("gas", "cell_mass"), "Msun")
+#phase_plot.save()
+
+# Create a line plot of gas temperature vs velocity_magnitude
+line_plot = yt.LinePlot(ds, [("gas", "temperature"), ("gas", "velocity_magnitude")], (-0.1, -0.1, -0.1), (0.1, 0.1, 0.1), 1000)
+line_plot.save()
